1dcbb37346c0cf3ee09443721081ce8a387e9925
Gonenc <mete@angelraise.com>
2019-06-12 13:16:22 +0300

    try 3

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Rails.application.configure do
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-end 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,6 +10,7 @@ module SunshineStreet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
